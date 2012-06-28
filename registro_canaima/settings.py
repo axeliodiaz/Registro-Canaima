8eab92b7f3460f74904a456ac3021ac6bc035f33
Axelio <diaz.axelio@gmail.com>
2012-06-27 22:42:53 -0430

    Configurando aplicación en PostgreSQL

--- registro_canaima/settings.py
+++ registro_canaima/settings.py
@@ -11,11 +11,11 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'base_de_datos.sql',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'registro_canaima',                      # Or path to database file if using sqlite3.
+        'USER': 'registro',                      # Not used with sqlite3.
+        'PASSWORD': 'registrocanaima',                  # Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
